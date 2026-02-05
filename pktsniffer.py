51ba32cb96ca4bc307c0f0aca19f1b335c9190cd
sm9614 <sm9614@rit.edu>
2026-02-04 21:53:56 -0500

    Ethernet headers
    Added a function that gets info from Ethernet headers

--- pktsniffer.py
+++ pktsniffer.py
@@ -1,17 +1,30 @@
 from scapy.all import rdpcap
+from scapy.layers.l2 import Ether
 import argparse
 
 
-def parse_args():
+def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-r', help='The pcap file being read')
-    parser.add_argument('c', type=int, help='The number of packets being read')
+    parser.add_argument('-c', type=int, help='The number of packets being read')
     parser.add_argument('filter', nargs='*',
                         help='filter types port, ip, tcp, udp, icmp, net')
-    return parser
+    return parser.parse_args()
+
+def get_ethernet_header(packet):
+    if packet.haslayer(Ether):
+        print(f'src:  {packet[Ether].src}')
+        print(f'dst:  {packet[Ether].dst}')
+        print(f'Packet Length: {len(packet)} bytes')
+        print(f'Ether Type: {(packet[Ether].type)} \n')
+
 
 def main():
-    args = parse_args()
-
+    args = get_args()
+    packets = rdpcap(args.r)
+    if args.c:
+        packets = packets[:args.c]
+    for p in packets:
+        get_etherneter_header(p)
 if __name__ == '__main__':
     main()
